969a2f8cdce552d411cf3dd8a0da4d1eb1421117
Benjamin Mark Lewis <benlewis.biology@gmail.com>
2025-12-11 21:02:49 +0000

    init fixed

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
 """  Bens wrapper to run GSEApy with reproducible FDR and save tables etc ... """
 
 from . import gseapy_pre_rank_wrap_v1 as rgw
+
+from .gseapy_pre_rank_wrap_v1 import (
+    run_gseapy_prerank_multiple_term_collections,
+    process_input_table,
+    run_single_gsea_prerank,
+)
